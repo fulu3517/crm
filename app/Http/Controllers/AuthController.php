93c9217486d4ebe9199e548bd2465fcaccbf3476
Fatih <fulu3517@gmail.com>
2021-08-19 19:23:49 +0300

    Register validasyon, User create, Token create, Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,53 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
+use Illuminate\Support\Carbon;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
         $request->validate([
-            'name'=>'required'
+            'name'=>'required',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|string|confirmed'
         ]);
+
+        $user = new User([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>md5($request->password)
+        ]);
+        $user->save();
+
+        $crendentials = [
+                            'email'=>$request->email,
+                            'passwor'=> $request->password
+        ];
+
+        if(!Auth::attempt($crendentials)) {
+            return response()->json([
+                'message' => 'Lütfen giriş bilgilerinizi kontrol ediniz'
+            ], 401);
+        }
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('Personal Access');
+        $token = $tokenResult->token;
+
+        if($request->remember_me) {
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        }
+        $token->save();
+
+        return response()->json([
+            'success' => true,
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'access_token' => $tokenResult->acccesToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ], 201);
     }
 }
