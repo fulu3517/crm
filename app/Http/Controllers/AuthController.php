2cb45b7b9aa76e9a8a48ed14ff1a46d5c4f26d0d
Fatih <fulu3517@gmail.com>
2021-08-19 18:45:34 +0300

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+            'name'=>'required'
+        ]);
+    }
+}
---
+++ resources/js/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+export default class Index extends Component {
+    render() {
+        return (
+            <div>
+                    merhaba DEneem
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Index/>,document.getElementById("index"))
